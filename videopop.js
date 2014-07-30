40fb4c9947755a8a4481fb8d8a21e63d68ab108f
Zuodong Yu <richzyu@gmail.com>
2014-07-30 14:04:27 +0800

    teat

--- videopop.js
+++ videopop.js
@@ -1,9 +1,9 @@
 (function(){curl=window.location.href;
-if (curl.search(/bilibili/)>0){
-	nurl="http://static.hdslb.com/miniloader.swf?aid="+curl.match(/\d+/g)+"&page=1";
-}else{
-	nurl=document.getElementsByTagName("embed")[0].getAttribute("src");
-}
-window.open(nurl,"","width=500,height=350");
-document.getElementsByTagName("embed")[0].parentNode.removeChild(document.getElementsByTagName("embed")[0]);
-});
+	if (curl.search(/bilibili/)>0){
+		nurl="http://static.hdslb.com/miniloader.swf?aid="+curl.match(/\d+/g)+"&page=1";
+	}else{
+		nurl=document.getElementsByTagName("embed")[0].getAttribute("src");
+	}
+	window.open(nurl,"","width=500,height=350");
+	document.getElementsByTagName("embed")[0].parentNode.removeChild(document.getElementsByTagName("embed")[0]);
+})();
