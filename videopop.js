6a4a609745ff5d052d96f98403ac066eea383862
Zuodong Yu <richzyu@gmail.com>
2014-07-30 18:23:11 +0800

    add tudou support

--- videopop.js
+++ videopop.js
@@ -4,6 +4,8 @@
 		nurl="http://static.hdslb.com/miniloader.swf?aid="+window.location.pathname.match(/\d+/g)+"&page=1";
 	}else if (ht.search(/youku/)>0){
 		nurl="http://player.youku.com/player.php/Type/Folder/Fid/"+window.location.href.match(/\d{5,}/g)+"/Ob/1/sid/"+window.location.href.match(/id_\w+/g).toString().substr(3)+"/v.swf";
+	}else if (ht.search(/tudou/)>0){
+		nurl="http://www.tudou.com/v/"+window.location.pathname.match(/\w+/g)[2]+"/v.swf";
 	}else{
 		nurl=document.getElementsByTagName("embed")[0].getAttribute("src");
 		document.getElementsByTagName("embed")[0].parentNode.removeChild(document.getElementsByTagName("embed")[0]);
