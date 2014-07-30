e3a6eccffee3437c61262285847d1a09c011ae9f
Zuodong Yu <richzyu@gmail.com>
2014-07-30 16:42:32 +0800

    add youku support

--- videopop.js
+++ videopop.js
@@ -2,7 +2,6 @@
 	ht=window.location.host;
 	if (ht.search(/bilibili/)>0){
 		nurl="http://static.hdslb.com/miniloader.swf?aid="+window.location.pathname.match(/\d+/g)+"&page=1";
-		document.getElementsByClassName("player")[0].parentNode.removeChild(document.getElementsByClassName("player")[0]);
 	}else if (ht.search(/youku/)>0){
 		nurl="http://player.youku.com/player.php/Type/Folder/Fid/"+window.location.href.match(/\d{5,}/g)+"/Ob/1/sid/"+window.location.href.match(/id_\w+/g).toString().substr(3)+"/v.swf";
 	}else{
