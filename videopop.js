efbd95c0d88f7cf68dded412c5f3cfb18085dd41
Zuodong Yu <richzyu@gmail.com>
2014-07-30 14:29:58 +0800

    test

--- videopop.js
+++ videopop.js
@@ -2,9 +2,10 @@
 	ht=window.location.host;
 	if (ht.search(/bilibili/)>0){
 		nurl="http://static.hdslb.com/miniloader.swf?aid="+window.location.pathname.match(/\d+/g)+"&page=1";
+		document.getElementsByClassName("player")[0].parentNode.removeChild(document.getElementsByClassName("player")[0]);
 	}else{
 		nurl=document.getElementsByTagName("embed")[0].getAttribute("src");
+		document.getElementsByTagName("embed")[0].parentNode.removeChild(document.getElementsByTagName("embed")[0]);
 	}
 	window.open(nurl,"","width=500,height=350");
-	document.getElementsByTagName("embed")[0].parentNode.removeChild(document.getElementsByTagName("embed")[0]);
 })();
