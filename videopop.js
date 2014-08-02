1bf6b15144392c050877765cc7e16f2d3dac7c38
Zuodong Yu <richzyu@gmail.com>
2014-08-02 19:17:11 +0800

    use URL

--- videopop.js
+++ videopop.js
@@ -1,18 +1,30 @@
 (function(){
-	ht=window.location.host;
+	//ht=window.location.host;
+	ht=document.URL
+	tt=document.title
 	if (ht.search(/bilibili/)>0){
-		nurl="http://static.hdslb.com/miniloader.swf?aid="+window.location.pathname.match(/\d+/g)+"&page=1";
+		nurl="http://static.hdslb.com/miniloader.swf?aid="+ht.match(/\d+/g)+"&page=1";
 	}else if (ht.search(/youku/)>0){
-		nurl="http://player.youku.com/player.php/Type/Folder/Fid/"+window.location.href.match(/\d{5,}/g)+"/Ob/1/sid/"+window.location.href.match(/id_\w+/g).toString().substr(3)+"/v.swf";
+		nurl="http://player.youku.com/player.php/Type/Folder/Fid/"+window.location.href.match(/\d{5,}/g)+"/Ob/1/sid/"+ht.match(/id_\w+/g).toString().substr(3)+"/v.swf";
 	}else if (ht.search(/tudou/)>0){
-		nurl="http://www.tudou.com/v/"+window.location.pathname.match(/\w+/g)[2]+"/v.swf";
+		nurl="http://www.tudou.com/v/"+ht.match(/\w+/g)[6]+"/v.swf";
 	}else if (ht.search(/youtube/)>0){
-		nurl="https://www.youtube.com/embed/"+window.location.href.match(/\w+/g)[6];
+		nurl="https://www.youtube.com/embed/"+ht.match(/\w+/g)[6];
 	}else if (ht.search(/acfun/)>0){
-		nurl="http://static.acfun.tv/player/ACFlashPlayer.out.swf?type=page&url="+window.location.href;
+		nurl="http://static.acfun.tv/player/ACFlashPlayer.out.swf?type=page&url="+ht;
 	}else{
 		nurl=document.getElementsByTagName("embed")[0].getAttribute("src");
 		document.getElementsByTagName("embed")[0].parentNode.removeChild(document.getElementsByTagName("embed")[0]);
 	}
-	window.open(nurl,"","width=520,height=325");
+	w=window.open(nurl,"","width=520,height=325");
+	//w.onload = function() { this.document.title = "your new title"; }
+	//document.addEventListener('click', function(e) {
+		//e = e || window.event;
+		//var target = e.target || e.srcElement,
+		//text = target.textContent || text.innerText;  
+		//var w=window.open(nurl,"","width=520,height=325");
+		//w.document.URL="http://www.google.com";
+		//w.document.write('<title>'+tt+'</title>');
+		//w.document.write(text);
+	//}, false);
 })();
