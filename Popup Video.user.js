bd1eee6ce01bba7e5a41d078031ad23ffc1d4cc7
Zuodong Yu <richzyu@gmail.com>
2014-09-09 20:08:40 +0800

    bugfix

--- Popup Video.user.js
+++ Popup Video.user.js
@@ -1,9 +1,8 @@
-
 // ==UserScript==
 // @name       Popup Video
 // @homepageURL https://github.com/archion/videopop
 // @namespace  https://github.com/archion/
-// @version    0.1.12
+// @version    0.1.13
 // @description  Popup online video out of the webpage
 // @match      http://*/*
 // @match      https://*/*
@@ -47,7 +46,20 @@ ck.addEventListener('click', function() {
 			//var target = e.target || e.srcElement;
 			//var target = e.target.cloneNode(true)
 			//var target = document.elementFromPoint(e.clientX,e.clientY).parentNode.cloneNode(true);
-			var target = document.elementFromPoint(e.clientX,e.clientY).cloneNode(true);
+            var target = document.elementFromPoint(e.clientX,e.clientY);
+            var ch=window.getComputedStyle(target,null).getPropertyValue("height");
+            var cw=window.getComputedStyle(target,null).getPropertyValue("width");
+            var ptarget=target;
+            var player = ptarget.cloneNode(true);
+            var cst="width:100% !important;height:100% !important;max-width:100% !important;margin:0px !important;padding:0px !important;top:0px !important;left:0px !important;z-index:10000 !important;overflow:hidden !important;position:fixed !important;display: block !important;visibility: visible !important;";
+            player.setAttribute("style",cst);
+            while(window.getComputedStyle(target.parentNode,null).getPropertyValue("height")==ch&&window.getComputedStyle(target.parentNode,null).getPropertyValue("width")==cw) {
+                ptarget=target.parentNode.cloneNode(true);
+                ptarget.replaceChild(player.cloneNode(true),ptarget.childNodes[Array.prototype.indexOf.call(target.parentNode.childNodes,target)]);
+                ptarget.setAttribute("style",cst);
+                player=ptarget;
+                target=target.parentNode;
+            }
 			var wurl=window.location.href;
 			if(ff){
 				wurl="about:blank";
@@ -63,18 +75,19 @@ ck.addEventListener('click', function() {
 						//w.document.write('<script type="text/undefined">');
 						//w.document.documentElement.replaceChild(document.head.cloneNode(true),w.document.head);
 						w.document.documentElement.innerHTML=" ";
+                        w.document.documentElement.replaceChild(document.head.cloneNode(true),w.document.head);
 						//w.document.head.parentNode.appendChild(document.createElement("body"));
 						//w.document.head.parentNode.removeChild(w.document.body);
 						//w.document.head.parentNode.appendChild(document.createElement("body"));
 						//w.document.documentElement.replaceChild(document.body.cloneNode(true),w.document.body);
 						//w.document.documentElement.replaceChild(document.head.cloneNode(true),w.document.head);
-						var hd=document.head.cloneNode(true).querySelectorAll(":not(script)");
-						for (var i=0, len=hd.length; i<len ;i++){
-							w.document.head.appendChild(hd[i]);
-						}
+						//var hd=document.head.cloneNode(true).querySelectorAll(":not(script)");
+						//for (var i=0, len=hd.length; i<len ;i++){
+							//w.document.head.appendChild(hd[i]);
+						//}
 						//var s = document.createElement('style');
 						//s.type = 'text/css';
-						//s.innerHTML = 'div { display: none; }';
+						//s.innerHTML = '* { height: 100% !important; width: 100%!important; }';
 						//w.document.head.appendChild(s);
 						//target.setAttribute("style","height: 100%; width: 100%; left: 0px; top: 0px;")
 						//target.style.height="100%";
@@ -84,11 +97,10 @@ ck.addEventListener('click', function() {
 						//target.style.margin="0px";
 						//target.style.padding="0px";
 						//w.document.body.removeAttribute("class");
-						target.id="archion";
-						target.className="archion";
-						target.setAttribute("style","height: 100% !important; width: 100% !important;margin: 0px !important;padding: 0px !important;border: 0px !important;min-width:90% !important;min-height:90% !important; left: 0px !important; top: 0px !important;visibility :visible !important;display :block !important");
-						w.document.body.className="archion";
-						w.document.body.setAttribute("style","height: 100% !important; width: 100% !important;margin: 0px !important;padding: 0px !important;border: 0px !important;min-width:90% !important;min-height:90% !important; left: 0px !important; top: 0px !important;display :block !important");
+						//target.id="video_box";
+						//target.className="video_box";
+						//w.document.body.className="popupvideo";
+						w.document.body.setAttribute("style",cst);
 						//var d=document.createElement("div");
 						//d.setAttribute("style","height: 100%; width: 100%; left: 0px; top: 0px;display: block;");
 						//d.appendChild(target);
@@ -97,7 +109,7 @@ ck.addEventListener('click', function() {
 						//d.appendChild(js[i]);
 						//}
 						//w.document.body.appendChild(d);
-						w.document.body.appendChild(target);
+						w.document.body.appendChild(player);
 						var rt=document.createElement('script');
 						rt.type = "text/javascript";
 						rt.innerHTML='var ck=document.createElement("div"); ck.setAttribute("style","position: fixed; top: 5px; right: 10px;z-index: 999999;background: #000; opacity: 0.3;color:#fff;font-size: 8pt;cursor: pointer;text-align: center;color:#fff;padding: 4px"); ck.innerHTML="return"; ck.onclick=function() { window.open("'+window.location.href+'"); window.close(); };document.body.appendChild(ck);';
--- videopop.js
+++ videopop.js
@@ -29,7 +29,20 @@ document.addEventListener('mousedown', function(e) {
 		//var target = e.target || e.srcElement;
 		//var target = e.target.cloneNode(true)
 		//var target = document.elementFromPoint(e.clientX,e.clientY).parentNode.cloneNode(true);
-		var target = document.elementFromPoint(e.clientX,e.clientY).cloneNode(true);
+		var target = document.elementFromPoint(e.clientX,e.clientY);
+		var ch=window.getComputedStyle(target,null).getPropertyValue("height");
+		var cw=window.getComputedStyle(target,null).getPropertyValue("width");
+		var ptarget=target;
+		var player = ptarget.cloneNode(true);
+		var cst="width:100% !important;height:100% !important;max-width:100% !important;margin:0px !important;padding:0px !important;top:0px !important;left:0px !important;z-index:10000 !important;overflow:hidden !important;position:fixed !important;display: block !important;visibility: visible !important;";
+		player.setAttribute("style",cst);
+		while(window.getComputedStyle(target.parentNode,null).getPropertyValue("height")==ch&&window.getComputedStyle(target.parentNode,null).getPropertyValue("width")==cw) {
+			ptarget=target.parentNode.cloneNode(true);
+			ptarget.replaceChild(player.cloneNode(true),ptarget.childNodes[Array.prototype.indexOf.call(target.parentNode.childNodes,target)]);
+			ptarget.setAttribute("style",cst);
+			player=ptarget;
+			target=target.parentNode;
+		}
 		var wurl=window.location.href;
 		if(ff){
 			wurl="about:blank";
@@ -45,18 +58,19 @@ document.addEventListener('mousedown', function(e) {
 					//w.document.write('<script type="text/undefined">');
 					//w.document.documentElement.replaceChild(document.head.cloneNode(true),w.document.head);
 					w.document.documentElement.innerHTML=" ";
+					w.document.documentElement.replaceChild(document.head.cloneNode(true),w.document.head);
 					//w.document.head.parentNode.appendChild(document.createElement("body"));
 					//w.document.head.parentNode.removeChild(w.document.body);
 					//w.document.head.parentNode.appendChild(document.createElement("body"));
 					//w.document.documentElement.replaceChild(document.body.cloneNode(true),w.document.body);
 					//w.document.documentElement.replaceChild(document.head.cloneNode(true),w.document.head);
-					var hd=document.head.cloneNode(true).querySelectorAll(":not(script)");
-					for (var i=0, len=hd.length; i<len ;i++){
-						w.document.head.appendChild(hd[i]);
-					}
+					//var hd=document.head.cloneNode(true).querySelectorAll(":not(script)");
+					//for (var i=0, len=hd.length; i<len ;i++){
+					//w.document.head.appendChild(hd[i]);
+					//}
 					//var s = document.createElement('style');
 					//s.type = 'text/css';
-					//s.innerHTML = 'div { display: none; }';
+					//s.innerHTML = '* { height: 100% !important; width: 100%!important; }';
 					//w.document.head.appendChild(s);
 					//target.setAttribute("style","height: 100%; width: 100%; left: 0px; top: 0px;")
 					//target.style.height="100%";
@@ -66,11 +80,10 @@ document.addEventListener('mousedown', function(e) {
 					//target.style.margin="0px";
 					//target.style.padding="0px";
 					//w.document.body.removeAttribute("class");
-					target.id="archion";
-					target.className="archion";
-					target.setAttribute("style","height: 100% !important; width: 100% !important;margin: 0px !important;padding: 0px !important;border: 0px !important;min-width:90% !important;min-height:90% !important; left: 0px !important; top: 0px !important;visibility :visible !important;display :block !important");
-					w.document.body.className="archion";
-					w.document.body.setAttribute("style","height: 100% !important; width: 100% !important;margin: 0px !important;padding: 0px !important;border: 0px !important;min-width:90% !important;min-height:90% !important; left: 0px !important; top: 0px !important;display :block !important");
+					//target.id="video_box";
+					//target.className="video_box";
+					//w.document.body.className="popupvideo";
+					w.document.body.setAttribute("style",cst);
 					//var d=document.createElement("div");
 					//d.setAttribute("style","height: 100%; width: 100%; left: 0px; top: 0px;display: block;");
 					//d.appendChild(target);
@@ -79,11 +92,7 @@ document.addEventListener('mousedown', function(e) {
 					//d.appendChild(js[i]);
 					//}
 					//w.document.body.appendChild(d);
-					w.document.body.appendChild(target);
-					//var hd=document.head.cloneNode(true).querySelectorAll(":not(script)");
-					//for (var i=0, len=hd.length; i<len ;i++){
-					//w.document.head.appendChild(hd[i]);
-					//}
+					w.document.body.appendChild(player);
 					var rt=document.createElement('script');
 					rt.type = "text/javascript";
 					rt.innerHTML='var ck=document.createElement("div"); ck.setAttribute("style","position: fixed; top: 5px; right: 10px;z-index: 999999;background: #000; opacity: 0.3;color:#fff;font-size: 8pt;cursor: pointer;text-align: center;color:#fff;padding: 4px"); ck.innerHTML="return"; ck.onclick=function() { window.open("'+window.location.href+'"); window.close(); };document.body.appendChild(ck);';
