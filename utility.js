ef8415396f3f6af860325c7aa7af0d3d340534e3
Zuodong Yu <richzyu@gmail.com>
2014-09-13 11:37:30 +0800

    update

---
+++ utility.js
@@ -0,0 +1,33 @@
+var i=0;
+var img=document.querySelectorAll("img")
+for (i=0;i<img.length;i++){
+	var aimg=document.createElement("a");
+	aimg.href=img[i].src;
+	img[i].parentNode.insertBefore(aimg, img[i]);
+	aimg.appendChild(img[i])
+}
+var link=document.querySelectorAll("a");
+for (i=0;i<link.length;i++){
+	link[i].setAttribute("target","_blank")
+}
+var tp=document.querySelector("i.top")
+tp.addEventListener('click',function(e){
+	var sm=function(){
+		if((document.body.scrollTop||document.documentElement.scrollTop)==0){
+			return;
+		}else{
+			scroll(0,(document.body.scrollTop||document.documentElement.scrollTop)*0.9);
+			setTimeout(sm,0.1);
+		}
+	};
+	sm();
+},false)
+window.onscroll=function(){
+	if((document.body.scrollTop||document.documentElement.scrollTop)>window.innerHeight/2){
+		document.querySelector("i.top").style.opacity=0.8;
+		document.querySelector("i.top").style.cursor="pointer";
+	}else{
+		document.querySelector("i.top").style.opacity=0;
+		document.querySelector("i.top").style.cursor="initial";
+	}
+}
