e1cb9482800ed4057172f8b6de29567672408127
Zuodong Yu <richzyu@gmail.com>
2014-09-13 11:20:32 +0800

    update

--- util.js
+++ util.js
@@ -10,3 +10,24 @@ var link=document.querySelectorAll("a");
 for (i=0;i<link.length;i++){
 	link[i].setAttribute("target","_blank")
 }
+var tp=document.querySelector("i.top")
+tp.addEventListener('click',function(e){
+	var sm=function(){
+		if((document.body.scrollTop||document.documentElement.scrollTop)==0){
+			return;
+		}else{
+			scroll(0,(document.body.scrollTop||document.documentElement.scrollTop)-15);
+			setTimeout(sm,0.1);
+		}
+	};
+	sm();
+},false)
+window.onscroll=function(){
+	if((document.body.scrollTop||document.documentElement.scrollTop)>window.innerHeight/2){
+		document.querySelector("i.top").style.opacity=1;
+		document.querySelector("i.top").style.cursor="pointer";
+	}else{
+		document.querySelector("i.top").style.opacity=0;
+		document.querySelector("i.top").style.cursor="initial";
+	}
+}
